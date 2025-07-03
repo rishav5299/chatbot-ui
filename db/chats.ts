694bbe42bd6649b0c98f535bf92a29a00f09e09c
rishav5299 <60104962+rishav5299@users.noreply.github.com>
2025-07-03 16:40:55 +0530

    Update chats.ts

--- db/chats.ts
+++ db/chats.ts
@@ -6,7 +6,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const userMessage = req.body.message;
 
   try {
-    const flowiseResponse = await fetch("https://flowise-lab.enigmaa.in/api/v1/prediction/6a619533-06fd-4df6-a07c-88403bb5c418", {
+    const flowiseResponse = await fetch("http://flowise-lab.enigmaa.in/api/v1/prediction/6a619533-06fd-4df6-a07c-88403bb5c418", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
